0acafce01c88eff4df5c5e4cdabbcdac92ca11f6
SABER-ferdjani <28saber3ferdjani2005@gmail.com>
2025-08-04 15:54:17 +0100

    fexTow

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://saburu:28032005@myferstdatabase.luc787a.mongodb.net/?retryWrites=true&w=majority&appName=MyFerstDataBase")
+mongoose.connect("mongodb+srv://saburu:28032005@myferstdatabase.pvqkqeq.mongodb.net/?retryWrites=true&w=majority&appName=MyFerstDataBase")
 .then(() => {
     console.log("connected successfully");
 }).catch((error) => {
@@ -11,15 +11,19 @@ mongoose.connect("mongodb+srv://saburu:28032005@myferstdatabase.luc787a.mongodb.
 app.use(express.json());
 app.use(express.static('public'));
 
-const user = require("../models/user");
+const user = require("./models/user");
 
 app.use(express.urlencoded({extended:true}));
+app.get("/" , (req,res) => {
+    res.render("log.ejs");
+})
 app.post("/log" , async (req,res) => {
     const userLog = new user() ;
-    userLog.usernsme = req.body.usernsme;
+    userLog.username = req.body.usernsme;
     userLog.email = req.body.email;
     userLog.password = req.body.password;
     await userLog.save();
+    res.send("pageShop.ejs");
     
 });
 
